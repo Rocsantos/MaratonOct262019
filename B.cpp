310e5f94ed61f5404d6016c0c8e792cb5b615d84
Rocsantos <42697323+Rocsantos@users.noreply.github.com>
2019-10-25 20:58:12 -0300

    Pasta base para a maratona do dia 26/10/2019

---
+++ B.cpp
@@ -0,0 +1,17 @@
+/*
+# Maraton Oct 26 2019
+### Authors:
+        Marcos Paulo Amorim Ribeiro
+        Rodrigo Carvalho dos Santos
+
+### Institution:
+    UnB Faculdade do Gama
+
+### Thanks for reading
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //Código aqui amigo
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Maraton Oct 26 2019
+
+## Contest da Maratona
+[Maratona no Codeforces](link "Codeforces")
+
 ### Repositório para auxiliar nos exercícios disponibilizado no codeforces, da maratona da IFB
+
+[link]: https://codeforces.com/
