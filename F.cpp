96977f5a9d253c1e474d1d8f9f60a70429b8558b
Rocsantos <ro12062000@gmail.com>
2019-10-26 15:33:40 -0300

    O pora num sei string

--- F.cpp
+++ F.cpp
@@ -10,8 +10,29 @@
 ### Thanks for reading
 */
 #include<bits/stdc++.h>
+#define M 1000
 using namespace std;
 int main(){
-    //Código aqui amigo
+    int n, m;
+    cin >> n >> m;
+    char mineiro[n][M], portugues[n][M], frase[M], *aux;
+    for (int i = 0; i < n; i++){
+        cout << "a";
+        fgets(mineiro[i], M, stdin);
+        cout << "b";
+        fgets(portugues[i], M, stdin);
+    }
+    for (int i = 0; i < m; i++){
+        cout << "c";
+        fgets(frase, M, stdin);
+        for (int i = 0; i < strlen(frase); i++){
+            for (int i = 0; i < n; i++){
+                aux = strstr(frase, mineiro[i]);
+                strncpy (aux, portugues[i], strlen(portugues[i]));
+            }
+            cout << frase;
+        }
+    }
+    
     return 0;
 }
